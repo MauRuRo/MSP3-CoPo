bf5087183d7b69f3d8c21974270b55d883eb7a90
MauRuRo <m.rubenderoos@gmail.com>
2020-07-13 19:51:24 +0000

    set up basic structure for app.py file, based on mini project example

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import flask_pymongo
 from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = ''
+app.config["MONGO_URI"] = ''
+
+mongo = PyMongo(app)
+
+# INSERT APP ROUTES HERE
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+#REMEMEBER TO SET DEBUG TO FALSE WHEN DONE WITH PROJECT!
